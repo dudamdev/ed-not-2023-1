317cab6a81dff090c93e28a472354ec3fbeaefa7
Eduarda Matos <111284160+dudamdev@users.noreply.github.com>
2023-04-20 20:19:33 -0300

    Aula 20/04

---
+++ 19_pilha3.py
@@ -0,0 +1,30 @@
+from lib.stack import Stack
+
+# expr = "2 + (( 5 * ( 3 / 2 + 1) / 4 ))" expressão com a quantidade certa de parênteses
+# expr = "(2 + (( 5 * ( 3 / (2 + 1) / 4 ))" expressão com mais parênteses abertos do que fechados
+expr = "2 + (( 5 * ( 3 / 2 ) + 1)) / 4 ))" # expressão com mais parênteses fechados do que abertos
+
+
+print(f"EXPRESSÃO ANALISADA: {expr}")
+
+pilha = Stack()
+
+# Percorre a expresão em busca de parênteses
+for pos in range(len(expr)):
+    # Empilha a posição quando é encontrado um abre parênteses
+    if expr[pos] == "(":
+        pilha.push(pos)
+        # print(pilha)
+    
+    # Desempilha a posição do último abre parêntese empilhado
+    # quando um fecha parênteses é encontrado
+    elif expr[pos] == ")":
+        pos_abre = pilha.pop()
+        print(f"Parênteses aberto na posição {pos_abre} foi fechado na posição {pos}")
+        # print(pilha)
+
+# Verifica sobras na pilha
+while not pilha.is_empty():
+    pos_abre = pilha.pop()
+    print(f"ERRO: parêntese aberto na posição {pos_abre} não possui o fechamento correspondente")
+
